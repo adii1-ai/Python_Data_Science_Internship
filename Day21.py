a34729ccfb0a7f59f42af05bb139fe500ce222d8
adii1-ai <gagareaditya32@gmail.com>
2026-02-05 07:44:49 +0530

    Abstraction

---
+++ Day21.py
@@ -0,0 +1,44 @@
+# Abstraction
+# - Abstraction is nothing but Hiding the Important details and showing only the necessary details
+# - By using Abstraction we can hide Important details or buisness logic
+
+
+# abc -> Abstract Base Class
+# - Allows us to create abstract base class
+
+# ABC -> Base class
+# - It is a Base class for making other classes abstract.
+# - When we Inherit from ABC , our class becomes abstract class
+
+
+from abc import ABC, abstractmethod
+
+# parent class
+class Car(ABC):
+
+    def details(self,_brand,color,price):
+        self._brand=_brand
+        self.color=color
+        self.price=price
+
+        print(f"Brand = {self._brand} , Color = {self.color} and Price ={self.price}")
+
+
+    @abstractmethod
+    def start(self):
+        print("Car Start.....logical part")
+
+# child class
+class tesla(Car):
+    def carDetails(self,_brand,color,price):
+        self.details(_brand,color,price)
+    
+    # must be write absract method in child class
+    def start(self):
+        print("tesla is car started....")
+    
+tesla1=tesla()
+tesla1.start()
+tesla1.carDetails("Tesla","White","$92,223")
+
+# cant create instance of a abstract class
